16609214fcccb02feee49a0a40db6a9b43342059
Pedro <ponsvpedro@gmail.com>
2023-01-16 11:11:29 +0100

    Modified plot_all to be more granular

--- draw_stellgap.py
+++ draw_stellgap.py
@@ -2,47 +2,59 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import plotting_styles as ps
+from itertools import product
 
 
 def read_output(path):
     df = pd.read_csv(path, skiprows=[0], header=None, sep='\s+')
     if len(df.columns) == 4:   # xstgap
-        df.columns=['rho', 'Freq [kHz]', 'm', 'n']
-    elif len(df.columns) == 6: # xstgap_snd
-        df.columns=['rho', 'Freq [kHz]', 'm', 'n', 'unk1', 'unk2']
+        df.columns = ['rho', 'freq', 'm', 'n']
+    elif len(df.columns) == 6:  # xstgap_snd
+        df.columns = ['rho', 'freq', 'm', 'n', 'unk1', 'unk2']
     return df.sort_values(by=['n', 'm', 'rho'])
 
 
-def plot_nm(df, n, m, *args, ax=None,  **kwargs):
+def plot_nm(df, n, m, *args, ax=None, quantity='freq',  **kwargs):
     b = df[(df['n'] == n) & (df['m'] == m)]
     label = f"n={n}, m={m}"
     if ax is None:
-        plt.plot(b['rho'], b['Freq [kHz]'], *args, label=label, **kwargs)
+        plt.plot(b['rho'], b[quantity], *args, label=label, **kwargs)
     else:
-        ax.plot(b['rho'], b['Freq [kHz]'], *args, label=label, **kwargs)
+        ax.plot(b['rho'], b[quantity], *args, label=label, **kwargs)
 
 
-def plot_all(df, *args, ax=None, **kwargs):
-    if ax is None:
-        plt.plot('rho', 'Freq [kHz]', data=df, *args, label=None, **kwargs)
-    else:
-        ax.plot(df['rho'], df['Freq [kHz]'], *args, label=None, **kwargs)
+def plot_all(df, *args, ax=None, quantity='freq', **kwargs):
+    for n, m in list(product(pd.unique(df['n']), pd.unique(df['m']))):
+        data = df[(df['n'] == n) & (df['m'] == m)]
+        if ax is None:
+            plt.plot('rho', quantity, data=data, *
+                     args, label='_nolegend_', **kwargs)
+        else:
+            ax.plot('rho', quantity, data=data, *
+                    args, label='_nolegend_', **kwargs)
+    ax.legend()
+    ax.get_legend().remove()
 
 
 if __name__ == '__main__':
     path = "/home/pedro/Documents/stellgap_pruebas/test/testone/alfven_post"
     data = read_output(path)
 
-    with ps.rc_context(ps.pub_style_one):
+    plot = True
 
-        fig, ax = plt.subplots(1, 1)
+    if plot:
+        with ps.rc_context(ps.pub_style_one):
+            fig, ax = plt.subplots(1, 1)
+            plot_all(data, marker='.', ls='', color='k',
+                     ms=1, ax=ax, quantity='freq')
+            plot_nm(data, n=2, m=6, ls='', marker='.',
+                    color='r', ms=2, ax=ax, quantity='freq')
+            plot_nm(data, n=2, m=5, ls='', marker='.',
+                    color='b', ms=2, ax=ax, quantity='freq')
+            # plt.plot('rho', 'Freq [kHz]', 'or', data=data)
+            ax.legend()
+            ax.set(xlabel=r'$\rho$', ylabel='Freq [kHz]')
+        plt.show()
+        fig.savefig('tmp.pdf')
 
-        plot_all(data, '.k', ms=1, ax=ax)
-        plot_nm(data, n=2, m=6, ls='', marker='.', color='r', ms=2, ax=ax)
-        plot_nm(data, n=3, m=5, ls='', marker='.', color='b', ms=2, ax=ax)
-        # plt.plot('rho', 'Freq [kHz]', 'or', data=data)
-        ax.legend()
-        ax.set(xlabel=r'$\rho$', ylabel='Freq [kHz]')
-    plt.show()
-
-    fig.savefig('tmp.pdf')
+    print(pd.unique(data['m']))
